4c788dfe5dcac7959bcf169d89d4a1093d1907c3
al4nw31 <xfx888@hotmail.com>
2014-05-23 23:30:24 -0700

    commit

---
+++ src/com/example/remembergamefinalmikeykram/RememberGameMainActivity.java
@@ -0,0 +1,111 @@
+package com.example.remembergamefinalmikeykram;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+import android.os.Build;
+
+public class RememberGameMainActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+		getActionBar().hide();
+
+		// TextView t = (TextView)findViewById(R.id.taperino);
+		// Animation blink =
+		// AnimationUtils.loadAnimation(getApplicationContext(), R.anim.tap);
+		// t.startAnimation(blink);
+
+		blink();
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+		setContentView(R.layout.activity_remember_game_main);
+
+	}
+	
+
+	    
+	    
+	private void blink(){
+	    final Handler handler = new Handler();
+	    new Thread(new Runnable() {
+	        @Override
+	        public void run() {
+	        int timeToBlink = 1000;    //in milissegunds
+	        try{Thread.sleep(timeToBlink);}catch (Exception e) {}
+	            handler.post(new Runnable() {
+	                @Override
+	                    public void run() {
+	                    TextView txt = (TextView) findViewById(R.id.taperino);
+	                    if(txt.getVisibility() == View.VISIBLE){
+	                        txt.setVisibility(View.INVISIBLE);
+	                    }else{
+	                        txt.setVisibility(View.VISIBLE);
+	                    }
+	                    blink();
+	                }
+	                });
+	            }
+	        }).start();
+	    }
+
+	public void intro(View v) {
+		Intent i = new Intent(this, IntroScreen.class);
+		startActivity(i);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.remember_game_main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(
+					R.layout.fragment_remember_game_main, container, false);
+			return rootView;
+		}
+	}
+
+}
